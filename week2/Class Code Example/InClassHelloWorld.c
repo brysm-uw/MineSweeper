ede797825695ddfe91648780eda515b1f6b86051
brysm-uw <brysm@uw.edu>
2025-03-16 19:44:25 -0700

    Delete week2 directory

--- week2/Class Code Example/InClassHelloWorld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-int main(){
-    //I want to print Hello World - I need to use a printf()
-    printf("Hello World!! first line\n");
-    printf("Hello World!! second line\n");
-    return 0;
-}
